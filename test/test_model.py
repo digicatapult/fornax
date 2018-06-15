312399622005872c1f4a5214510fb76a6737e1e4
Dan-Staff <daniel.r.staff@gmail.com>
2018-06-15 10:07:52 +0100

    refactor test_model.py
    split TestEdge into individual tests.

--- test/test_model.py
+++ test/test_model.py
@@ -45,8 +45,8 @@ class TestNode(TestCaseDB):
 
 class TestEdge(TestCaseDB):
 
-    def test_edge_round_trip(self):
-        """ edge round trip """
+    def setUp(self):
+        super().setUp()
         new_edge_type = model.EdgeType(id=0, description="An edge type for testing")
         new_node_type = model.NodeType(id=0, description="A node type for testing")
         self.session.add(new_edge_type)
@@ -61,20 +61,27 @@ class TestEdge(TestCaseDB):
         for new_node in new_nodes:
             self.session.add(new_node)
         self.session.commit()
-        
+
         new_edge = model.Edge(start=0, end=1, type=0, weight=1.)
         self.session.add(new_edge)
         self.session.commit()
 
+    def test_edge_round_trip(self):
+        """ edge round trip """
+
         found = self.session.query(model.Edge).first()
         self.assertIsNotNone(found)
-        self.assertEqual(new_edge.start, found.start)
+        self.assertEqual(0, found.start)
+
+    def test_edge_join_start(self):
 
         query = self.session.query(model.Node)
         query = query.join(model.Edge, model.Node.id==model.Edge.start)
         found = query.first()
         self.assertIsNotNone(found)
         self.assertEqual(found.id, 0)
+
+    def test_edge_join_end(self):
 
         query = self.session.query(model.Node)
         query = query.join(model.Edge, model.Node.id==model.Edge.end)
