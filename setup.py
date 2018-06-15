eb9c4717518ed1a5213b888e4b2b2c8e7d5aa0b5
Dan-Staff <daniel.r.staff@gmail.com>
2018-06-15 09:12:43 +0100

    project outline

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'fornax',
+    version = '0.0.0',
+    author = 'Daniel Staff',
+    author_email = 'daniel.staff@digicatapult.org.uk',
+    description = '',
+    packages = find_packages(),    
+    install_requires = [],
+)
