3260603664e5f03c8e2327ee56f11dead28aafec
Dan-Staff <daniel.r.staff@gmail.com>
2018-06-15 09:20:25 +0100

    New select submodule

---
+++ fornax/select.py
@@ -0,0 +1,33 @@
+from typing import List
+from sqlalchemy.orm import Query, aliased
+from sqlalchemy.sql import func, select, text
+from fornax.model import Node, Edge, EdgeType, NodeType
+
+
+def get_candidate(distance: float, label: str) -> Query:
+    """ 
+        return a sqlalchemy query object to fuzzy 
+        match a query node label to target node labels
+    """
+
+    if not 0 <= distance < 1:
+        raise ValueError("distances must be between zero and one")
+    query = Query(Node)
+    query = query.filter(Node.label.op('<->')(label) < distance)
+    return query
+
+
+def get_neighbours(query: Query) -> Query:
+    """ 
+        starting with a query that selects a table of nodes
+        return a query that returns all of the neighbours
+        of each node
+
+        query: a query that selects a table a nodes
+        returns: a query selecting a table of nodes
+    """
+    
+    subquery = query.subquery()
+    query = Query(Node).join(Edge, Edge.end == Node.id)
+    query = query.filter(Edge.start == subquery.c.id)
+    return query
---
+++ test/test_select.py
@@ -0,0 +1,78 @@
+import unittest
+import fornax.model as model
+from sqlalchemy.sql import func
+from sqlalchemy.orm import Query
+from test_base import TestCaseDB
+
+import fornax.select
+
+class TestSelect(TestCaseDB):
+
+    def setUp(self):
+        """ populate the database """
+        super().setUp()
+        new_node_type = model.NodeType(id=0, description="A node type for testing")
+        new_edge_type = model.EdgeType(id=0, description="An edge type for testing")
+        self.session.add(new_node_type)
+        self.session.add(new_edge_type)
+        self.session.commit()
+
+        # Insert some labelled nodes
+        labels = ['Matt', 'Dom', 'Callum', 'David', 'Anthony']
+        for id_, label in enumerate(labels):
+            new_node = model.Node(id=id_, label=label, type=0)
+            self.session.add(new_node)
+            self.session.commit()
+
+        self.session.add(model.Edge(start=0, end=1, type=0, weight=1))
+        self.session.add(model.Edge(start=2, end=3, type=0, weight=1))
+
+
+    def test_get_candidate(self):
+        """ test get candidate """
+        query = fornax.select.get_candidate(0.7, 'Mat')
+        nodes = query.with_session(self.session).all()
+        self.assertEqual(
+            sorted([node.label for node in nodes]), 
+            sorted(['Matt'])
+        )
+
+    def test_get_candidates_empty(self):
+        """ test get_candidates with empty string """
+        query = fornax.select.get_candidate(0.7, [])
+        nodes = query.with_session(self.session).all()
+        self.assertEqual([node.label for node in nodes], [])
+
+    def test_get_candidates_negative_distance(self):
+        """ test get candidate with negative distance"""
+        self.assertRaises(ValueError, fornax.select.get_candidate, -1, ['Matt'])
+
+    def test_get_candidates_distance_gt_one(self):
+        """ test get candidate with distance .gt. one"""
+        self.assertRaises(ValueError, fornax.select.get_candidate, 1.1, ['Matt'])
+
+    def test_get_neighbours(self):
+        """ test get neighbours """
+        query = fornax.select.get_candidate(0.7, 'Matt')
+        query = fornax.select.get_neighbours(query)
+        nodes = query.with_session(self.session).all()
+        self.assertEqual([node.label for node in nodes], ['Dom'])
+
+    def test_get_many_neighbours(self):
+        """ test get many neighbours """
+        query = self.session.query(model.Node).filter(model.Node.id < 2)
+        query = fornax.select.get_neighbours(query)
+        nodes = query.with_session(self.session).all()
+        self.assertEqual([node.label for node in nodes], ['Dom'])
+
+    def test_get_neighbours_reccursive(self):
+        """ get next nearest neighbours """
+        #TODO: Move reccursion outside of testing 
+        query = fornax.select.get_candidate(0.7, 'Matt')
+        query = fornax.select.get_neighbours(query).union(
+            fornax.select.get_neighbours(
+                fornax.select.get_neighbours(query)
+            )
+        )
+        nodes = query.with_session(self.session).all()
+        self.assertEqual([node.label for node in nodes], ['Dom'])
