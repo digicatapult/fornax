cf8f719e9a57c845642f48a7ddb017cfd72a8c0d
Dan-Staff <daniel.r.staff@gmail.com>
2018-06-18 16:24:36 +0100

    New submodule opt
    New functions proximity and delta_plus

---
+++ fornax/opt.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def proximity(h: float, alpha: float, distances: np.ndarray) -> np.ndarray:
+    """Calculates the proximity factor P for an array of distances.
+    Implements equation 1 in the paper
+    
+    Arguments:
+        h {float} -- max hopping distance
+        alpha {float} -- propagation factor
+        distances {np.array} -- array of hopping distances
+    
+    Raises:
+        ValueError -- if hopping distance is less than zero
+        ValueError -- if propagation factor is not between zero and one
+    
+    Returns:
+        np.array -- an array of proximiy values
+    """
+
+    if h < 0:
+        raise ValueError('hopping distance h cannot be negative')
+    if not 0 < alpha <= 1:
+        raise ValueError('propagation factor alpha must be between 0 and 1')
+    return np.multiply(
+        np.less_equal(distances, h),
+        np.power(alpha, distances)
+    )
+
+def delta_plus(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """Comparator function. Equation 3 in the paper.
+    
+    Arguments:
+        x {np.array} -- an array of floats
+        y {np.array} -- an array of floats
+    
+    Returns
+        np.array -- an array of floats
+    """
+
+    return np.multiply(
+        np.greater(x, y), 
+        np.subtract(x, y)
+    )
