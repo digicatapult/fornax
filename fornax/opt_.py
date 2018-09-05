bfea84f10fba3e8fc366a667737c9dfeebcc9c58
Dan-Staff <daniel.r.staff@gmail.com>
2018-09-05 12:48:08 +0100

    try subclassing recarray

--- fornax/opt_.py
+++ fornax/opt_.py
@@ -1,36 +1,242 @@
-import abc
 import numpy as np
 
-class Frame:
 
-    def __init__(self, records, names):
-        self.records = np.rec.fromrecords(records, names=names)
+class Base(np.recarray):
 
-    def __getitem__(self, indx) -> 'Frame':
-        return self.from_record_array(self.records[indx])
+    """A Base class for subclassing numpy record arrays
+    
+    Returns:
+        np.recarray -- A subclass of np.recarray
+    """
 
-    def __len__(self):
-        return len(self.records)
+    columns = []
+    types = []
 
-    @classmethod
-    def from_record_array(cls, rec_array):
-        new = cls.__new__(cls)
-        new.records = rec_array
-        return new
+    def __new__(cls, *args, **kwargs):
+        a = np.atleast_2d(np.array(*args, **kwargs))
+        dtype = np.dtype(list(zip(cls.columns, cls.types)))
+        r = np.recarray(shape=a.shape[0], dtype=dtype)
+        for i, col in enumerate(cls.columns):
+            setattr(r, col, a[:,i])
+        return r.view(NeighbourHoodMatchingCosts)
 
 
-class NeighbourHoodMatchingCosts(Frame):
+class NeighbourHoodMatchingCosts(Base):
 
-    names = 'u v uu vv cost'.split()
+    """Represents a table of all valid neighbourhood matching costs"""
 
-    def __init__(self, records):
-        super().__init__(records, self.names)
+    columns = 'v u vv uu cost'.split()
+    types = '<i8 <i8 <i8 <i8 <f8'.split()
+
+    def __getitem__(self, indx):
+        """Get the row representing the neighbourhood matching costs at index "indx"
+        
+        Arguments:
+            indx {int, slice} -- index into the table
+        
+        Returns:
+            NeighbourHoodMatchingCosts -- NeighbourHoodMatchingCosts sliced by indx
+        """
+        return super().__getitem__(indx)
+
+    @property
+    def v(self):
+        """Get column v
+        
+        Returns:
+            np.ndarray -- array of query node ids as integers
+        """
+
+        return getattr(super(), 'u')
 
     @property
     def u(self):
-        """Column of Query node ids u
+        """Get column u
         
         Returns:
-            [int] -- [return the column of query node ids u]
+            np.ndarray -- array of target node ids as integers
         """
-        return self.records.u
+
+        return getattr(super(), 'v')
+
+    @property
+    def vv(self):
+        """Get column vv - written v prime (v') in the paper where v' is a query node within
+        hopping distance h of query node v
+        
+        Returns:
+            np.ndarray -- array of query node ids as integers
+        """
+
+        return getattr(super(), 'vv')
+
+    @property
+    def uu(self):
+        """Get column uu - written u prime (u') in the paper where u' is a target node within
+        hopping distance h of target node u
+        
+        Returns:
+            np.ndarray -- array of target node ids as integers
+        """
+
+        return getattr(super(), 'uu')
+
+
+    @property
+    def cost(self):
+        """Get column cost - all valid neighbourhood matching costs.
+
+        Eq 2 in the paper - multiplied by 1 - lambda
+        
+        Returns:
+            np.ndarray -- array of costs and floats
+        """
+
+        return getattr(super(), 'cost')
+
+    def __repr__(self):
+        return 'NeighbourHoodMatchingCosts(records={}, dtypes={})'.format(
+            [record for record in self], self.types
+        )
+
+
+class PartialMatchingCosts(Base):
+
+    """ A table representing all valid partial matching costs """
+
+    columns = 'v u vv cost'.split()
+    types = '<i8 <i8 <i8 <f8'.split()
+
+    @property
+    def v(self):
+        """Get column v
+        
+        Returns:
+            np.ndarray -- array of query node ids as integers
+        """
+
+        return getattr(super(), 'v')
+
+    @property
+    def u(self):
+        """Get column u
+        
+        Returns:
+            np.ndarray -- array of target node ids as integers
+        """
+
+        return getattr(super(), 'u')
+
+    @property
+    def vv(self):
+        """Get column vv - written v prime (v') in the paper where v' is a query node within
+        hopping distance h of query node v
+        
+        Returns:
+            np.ndarray -- array of query node ids as integers
+        """
+
+        return getattr(super(), 'uu')
+
+    @property
+    def cost(self):
+        """Get column cost - all valid partial matching costs.
+
+        Eq 13 in the paper (W) - but with beta multiplied by a factor of 1 - lambda
+        
+        Returns:
+            np.ndarray -- array of costs as floats
+        """
+
+        return getattr(super(), 'cost')
+
+    def __repr__(self):
+        return 'PartialMatchingCosts(records={}, dtypes={})'.format(
+            [record for record in self], self.types
+        )
+
+
+class InferenceCost(Base):
+
+    """ A table representing all valid inference costs between query node
+    u and target node v"""
+
+    columns = 'v u cost'.split()
+    types = '<i8 <i8 <f8'.split()
+
+    @property
+    def v(self):
+        """Get column v
+        
+        Returns:
+            np.ndarray -- array of query node ids as integers
+        """
+
+        return getattr(super(), 'v')
+
+    @property
+    def u(self):
+        """Get column u
+        
+        Returns:
+            np.ndarray -- array of target node ids as integers
+        """
+
+        return getattr(super(), 'u')
+    
+    @property
+    def cost(self):
+        """Get column cost - all valid inference costs for query node v and target node u.
+
+        Eq 14 in the paper (U)
+        
+        Returns:
+            np.ndarray -- array of costs as floats
+        """
+
+        return getattr(super(), 'cost')
+
+    def __repr__(self):
+        return 'InferenceCost(records={}, dtypes={})'.format(
+            [record for record in self], self.types
+        )
+
+
+class OptimalMatch(Base):
+
+    """Table representing the cost of the optimal match for query node v going to u"""
+
+    columns = 'v u cost'.split()
+    types = '<i8 <i8 <f8'.split()
+
+    @property
+    def v(self):
+        """Get column v
+        
+        Returns:
+            np.ndarray -- array of query node ids as integers
+        """
+
+        return getattr(super(), 'v')
+
+    @property
+    def u(self):
+        """Get column u
+        
+        Returns:
+            np.ndarray -- array of target node ids as integers
+        """
+
+        return getattr(super(), 'u')
+
+    @property
+    def cost(self):
+        """Get column cost - the optimal matching cost for u going to v.
+
+        Eq 10 in the paper (O)
+        
+        Returns:
+            np.ndarray -- array of costs as floats
+        """
+
+        return getattr(super(), 'cost')
--- test/test_opt_.py
+++ test/test_opt_.py
@@ -2,48 +2,19 @@ import unittest
 import fornax.opt_
 import numpy as np
 
-
-class Dummy(fornax.opt_.Frame):
-
-    names = 'a b c'.split()
-
-    def __init__(self, records):
-        super().__init__(records, self.names)
-
-    @property
-    def a(self):
-        return self.records.a
-
-    @property
-    def b(self):
-        return self.records.b
-
-
-class TestFrame(unittest.TestCase):
-
-    def setUp(self):
-        self.names = 'a b'
-        self.records = [[1, 2, 3], [2, 4, 6]]
-        self.dummy = Dummy(self.records)
-
-    def test_get_item(self):
-        self.assertEqual(self.dummy[0].a, self.records[0][0])
-        self.assertEqual(self.dummy[0].b, self.records[0][1])
-        self.assertEqual(self.dummy[1].a, self.records[1][0])
-
-    def test_len(self):
-        self.assertEqual(len(self.dummy), 2)
-
-    def test_slice(self):
-        self.assertTrue(isinstance(self.dummy[:1], Dummy))
-        
-
 class TestNeighbourHoodMatchingCosts(unittest.TestCase):
 
     def setUp(self):
         self.records = [[1, 2, 3, 4, 5], [2, 4, 6, 7, 8]]
         self.costs = fornax.opt_.NeighbourHoodMatchingCosts(self.records)
 
+    def test_get_v(self):
+        self.assertListEqual(list(self.costs.v), [1, 2])
+        self.assertEqual(self.costs[0].v, 1)
+
     def test_get_u(self):
-        self.assertListEqual(list(self.costs.u), [1, 2])
-        self.assertEqual(self.costs[0].u, 1)
+        self.assertListEqual(list(self.costs.u), [2, 4])
+        self.assertEqual(self.costs[0].u, 2)
+
+    def test_type(self):
+        self.assertTrue(isinstance(self.costs[:1], fornax.opt_.NeighbourHoodMatchingCosts))
