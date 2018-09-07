766d952e37e0004e1658f155c2d1a24c870d154d
Dan-Staff <daniel.r.staff@gmail.com>
2018-09-07 10:56:07 +0100

    reinstate some tests

--- test/test_opt.py
+++ test/test_opt.py
@@ -1,12 +1,55 @@
 import unittest
-import fornax.opt_
+import fornax.opt as opt
 import numpy as np
+
+class TestProximity(unittest.TestCase):
+
+    def setUp(self):
+        self.h = 2
+        self.alpha = .3
+
+    def test_zero(self):
+        proximities = opt._proximity(self.h, self.alpha, np.array([0]))
+        self.assertListEqual([1.0], proximities.tolist())
+
+    def test_one(self):
+        proximities = opt._proximity(self.h, self.alpha, np.array([1]))
+        self.assertListEqual([self.alpha], proximities.tolist())
+
+    def test_pow(self):
+        proximities = opt._proximity(self.h, self.alpha, np.array([2, 3, 4]))
+        self.assertListEqual([self.alpha**2, 0.0, 0.0], proximities.tolist())
+
+    def test_assert_h(self):
+        self.assertRaises(ValueError, opt._proximity, -1, 0.3, np.array([0]))
+
+    def test_assert_alpha_big(self):
+        self.assertRaises(ValueError, opt._proximity, 2, 1.1, np.array([0]))
+
+    def test_assert_alpha_small(self):
+        self.assertRaises(ValueError, opt._proximity, 2, -.1, np.array([0]))
+
+
+class TestDeltaPlus(unittest.TestCase):
+
+    def test_greater(self):
+        self.assertListEqual(
+            opt._delta_plus(np.array([2, 4, 6]), np.array([1, 2, 3])).tolist(),
+            [1, 2, 3]
+        )
+
+    def test_less(self):
+        self.assertListEqual(
+            opt._delta_plus(np.array([1, 2, 3]), np.array([2, 4, 6])).tolist(),
+            [0, 0, 0]
+        )
+
 
 class TestNeighbourHoodMatchingCosts(unittest.TestCase):
 
     def setUp(self):
         self.records = [[1, 2, 3, 4, 5], [2, 4, 6, 7, 8]]
-        self.costs = fornax.opt_.NeighbourHoodMatchingCosts(self.records)
+        self.costs = opt.NeighbourHoodMatchingCosts(self.records)
 
     def test_get_v(self):
         self.assertListEqual(list(self.costs.v), [1, 2])
@@ -17,12 +60,64 @@ class TestNeighbourHoodMatchingCosts(unittest.TestCase):
         self.assertEqual(self.costs[0].u, 2)
 
     def test_type(self):
-        self.assertTrue(isinstance(self.costs[:1], fornax.opt_.NeighbourHoodMatchingCosts))
+        self.assertTrue(isinstance(self.costs[:1], opt.NeighbourHoodMatchingCosts))
 
+
+# class TestOpt(unittest.TestCase):
+
+#     def setUp(self):
+#         self.records = ([
+#             (1, 1, 1, 1, 0, 0, 1), (1, 1, 1, 4, 0, 1, 1), (1, 1, 2, 5, 1, 2, 1), 
+#             (1, 1, 3, 3, 1, 1, 1), (1, 1, 3, 6, 1, 2, 1), (1, 1, 4, 7, 2, 2, 1), 
+#             (1, 4, 1, 1, 0, 1, 1), (1, 4, 1, 4, 0, 0, 1), (1, 4, 1, 8, 0, 2, 1), 
+#             (1, 4, 2, 5, 1, 1, 1), (1, 4, 3, 3, 1, 2, 1), (1, 4, 3, 6, 1, 1, 1), 
+#             (1, 4, 4, 7, 2, 2, 1), (1, 8, 1, 4, 0, 2, 1), (1, 8, 1, 8, 0, 0, 1), 
+#             (1, 8, 2, 9, 1, 1, 1), (1, 8, 3, 6, 1, 1, 1), (1, 8, 3, 12, 1, 1, 1), 
+#             (1, 8, 4, 10, 2, 2, 1), (2, 5, 1, 1, 1, 2, 1), (2, 5, 1, 4, 1, 1, 1), 
+#             (2, 5, 2, 5, 0, 0, 1), (2, 5, 3, 3, 2, 2, 1), (2, 5, 3, 6, 2, 2, 1), 
+#             (2, 5, 4, 7, 1, 1, 1), (2, 5, 4, 10, 1, 2, 1), (2, 5, 5, None, 2, None, 1), 
+#             (2, 9, 1, 8, 1, 1, 1), (2, 9, 2, 9, 0, 0, 1), (2, 9, 3, 6, 2, 2, 1), 
+#             (2, 9, 3, 12, 2, 2, 1), (2, 9, 4, 7, 1, 2, 1), (2, 9, 4, 10, 1, 1, 1), 
+#             (2, 9, 5, 11, 2, 2, 1), (3, 3, 1, 1, 1, 1, 1), (3, 3, 1, 4, 1, 2, 1), 
+#             (3, 3, 2, 5, 2, 2, 1), (3, 3, 3, 3, 0, 0, 1), (3, 6, 1, 1, 1, 2, 1), 
+#             (3, 6, 1, 4, 1, 1, 1), (3, 6, 1, 8, 1, 1, 1), (3, 6, 2, 5, 2, 2, 1), 
+#             (3, 6, 2, 9, 2, 2, 1), (3, 6, 3, 6, 0, 0, 1), (3, 6, 3, 12, 0, 2, 1), 
+#             (3, 12, 1, 8, 1, 1, 1), (3, 12, 2, 9, 2, 2, 1), (3, 12, 3, 6, 0, 2, 1), 
+#             (3, 12, 3, 12, 0, 0, 1), (3, 12, 3, 13, 0, 2, 1), (3, 13, 1, None, 1, None, 1), 
+#             (3, 13, 2, None, 2, None, 1), (3, 13, 3, 12, 0, 2, 1), (3, 13, 3, 13, 0, 0, 1), 
+#             (4, 7, 1, 1, 2, 2, 1), (4, 7, 1, 4, 2, 2, 1), (4, 7, 2, 5, 1, 1, 1), 
+#             (4, 7, 2, 9, 1, 2, 1), (4, 7, 4, 7, 0, 0, 1), (4, 7, 4, 10, 0, 1, 1), 
+#             (4, 7, 5, 11, 1, 2, 1), (4, 10, 1, 8, 2, 2, 1), (4, 10, 2, 5, 1, 2, 1), 
+#             (4, 10, 2, 9, 1, 1, 1), (4, 10, 4, 7, 0, 1, 1), (4, 10, 4, 10, 0, 0, 1), 
+#             (4, 10, 5, 11, 1, 1, 1), (5, 11, 2, 9, 2, 2, 1), (5, 11, 4, 7, 1, 2, 1), 
+#             (5, 11, 4, 10, 1, 1, 1), (5, 11, 5, 11, 0, 0, 1)
+#         ])
+#         self.query_result = fornax.opt.QueryResult([tuple(item if item is not None else -1 for item in tup) for tup in self.records])
+
+
+#     def test_missed(self):
+#         _, misses = fornax.opt.missed(self.query_result)
+#         self.assertEqual(misses[(3, 13)], 2)
+#         self.assertEqual(misses[(2, 5)], 1)
+
+#     def test_totals(self):
+#         totals, _ = fornax.opt.missed(self.query_result)
+#         self.assertEqual(totals[(3, 13)], 1)
+#         self.assertEqual(totals[(2, 5)], 4)
+
+#     def test_get_matching_costs(self):
+#         matching_costs = fornax.opt.get_matching_costs(self.query_result)
+#         self.assertFalse(True)
+
+#     def test_solve(self):
+#         fornax.opt.solve(self.query_result)
 
 class TestOpt(unittest.TestCase):
+    """Reproduce the scenario set out in figure 4 of the paper"""
 
     def setUp(self):
+        self.h = 2
+        self.alpha = .3
         self.records = ([
             (1, 1, 1, 1, 0, 0, 1), (1, 1, 1, 4, 0, 1, 1), (1, 1, 2, 5, 1, 2, 1), 
             (1, 1, 3, 3, 1, 1, 1), (1, 1, 3, 6, 1, 2, 1), (1, 1, 4, 7, 2, 2, 1), 
@@ -49,22 +144,20 @@ class TestOpt(unittest.TestCase):
             (4, 10, 5, 11, 1, 1, 1), (5, 11, 2, 9, 2, 2, 1), (5, 11, 4, 7, 1, 2, 1), 
             (5, 11, 4, 10, 1, 1, 1), (5, 11, 5, 11, 0, 0, 1)
         ])
-        self.query_result = fornax.opt_.QueryResult([tuple(item if item is not None else -1 for item in tup) for tup in self.records])
+        self.query_result = opt.QueryResult([tuple(item if item is not None else -1 for item in tup) for tup in self.records])
 
 
-    def test_missed(self):
-        _, misses = fornax.opt_.missed(self.query_result)
-        self.assertEqual(misses[(3, 13)], 2)
-        self.assertEqual(misses[(2, 5)], 1)
+    def test_optimal_matches(self):
 
-    def test_totals(self):
-        totals, _ = fornax.opt_.missed(self.query_result)
-        self.assertEqual(totals[(3, 13)], 1)
-        self.assertEqual(totals[(2, 5)], 4)
+        graphs, scores = opt.solve(self.query_result)
+        perfect = [graph for graph, score in zip(graphs, scores) if score == 0]
 
-    def test_get_matching_costs(self):
-        matching_costs = fornax.opt_.get_matching_costs(self.query_result)
-        self.assertFalse(True)
+        self.assertSequenceEqual(
+            perfect[0],
+            [(1, 8), (2, 9), (3, 6), (4, 10), (5, 11)]
+        )
 
-    def test_solve(self):
-        fornax.opt_.solve(self.query_result)
+        self.assertSequenceEqual(
+            perfect[1],
+            [(1, 8), (2, 9), (3, 12), (4, 10), (5, 11)]
+        )
