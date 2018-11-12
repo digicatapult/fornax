f617657884a2fe47e00c9bc674b60e7deac1c37a
Dan-Staff <daniel.r.staff@gmail.com>
2018-11-12 14:20:32 +0000

    create, read, delete interface for model.Graph

--- fornax/api.py
+++ fornax/api.py
@@ -123,58 +123,19 @@ class GraphHandle:
         return self._graph_id
 
     @classmethod
-    def create(cls, nodes:Iterable, edges:Iterable, metadata=None): 
-        """Create a new graph and return a handle to it.
-        
-        Arguments:
-            nodes {Iterable} -- Iterable of unique integer node identifiers
-            edges {Iterable} -- Pairs of unique integer node identifiers being joined by an undirected edge
-        
-        Keyword Arguments:
-            metadata {dict} -- A dictionary of metadata about each node (must be serialisable to json) (default: {None})
-        
-        Returns:
-            GraphHandle -- Handle to a new graph
-        """
+    def create(cls): 
 
         with session_scope() as session:
         
-            query = session.query(sqlalchemy.func.max(model.Node.graph_id)).first()
+            query = session.query(sqlalchemy.func.max(model.Graph.graph_id)).first()
             graph_id = query[0]
             
             if graph_id is None:
                 graph_id = 0
             else:
                 graph_id += 1
-            
             session.add(model.Graph(graph_id=graph_id))
             session.commit()
-            
-            if metadata is not None:
-
-                session.add_all(
-                    model.Node(node_id=node_id, graph_id=graph_id, meta=json.dumps(meta)) 
-                    for node_id, meta in zip(check_nodes(nodes), metadata)
-                )
-            else:
-                session.add_all(
-                    model.Node(node_id=node_id, graph_id=graph_id) 
-                    for node_id in check_nodes(nodes)
-                )
-
-            session.commit()
-
-            session.add_all(
-                itertools.chain.from_iterable(
-                    (
-                        model.Edge(start=start, end=end, graph_id=graph_id), 
-                        model.Edge(start=end, end=start, graph_id=graph_id)
-                    )
-                    for start, end in check_edges(edges)
-                )
-            )
-            session.commit()
-        
         return GraphHandle(graph_id)
 
     @classmethod
@@ -186,241 +147,13 @@ class GraphHandle:
         """ delete this graph from the database back end """
         self.check_exists()
         with session_scope() as session:
+            session.query(model.Graph).filter(model.Graph.graph_id==self._graph_id).delete()
             session.query(model.Edge).filter(model.Edge.graph_id==self._graph_id).delete()
             session.query(model.Node).filter(model.Node.graph_id==self._graph_id).delete()
 
     def check_exists(self):
         """ raise an exception if the graph handle is pointing to a graph that no longer exists """
         with session_scope() as session:
-            exists = session.query(sqlalchemy.exists().where(model.Node.graph_id==self._graph_id)).scalar()
+            exists = session.query(sqlalchemy.exists().where(model.Graph.graph_id==self._graph_id)).scalar()
         if not exists:
             raise ValueError('cannot read graph with graph id: {}'.format(self._graph_id))
-
-class QueryHandle:
-    """ Represents a handle to a query in the database back end.
-    A query is a tuple of 3 objects:
-        - a query graph
-        - a target graph
-        - a set of directed edges going from the query graph to the target graph
-    """
-
-
-    def __init__(self, query_id: int):
-        """Create a handle to a query with uniuque integer id `query_id`
-        
-        Arguments:
-            query_id {int} -- a uniuque integer id number for a query
-        """
-        self._query_id = query_id
-        self.check_exists()
-
-    @property
-    def query_id(self):
-        return self._query_id
-
-    @classmethod
-    def create(cls, start_graph: GraphHandle, end_graph: GraphHandle, matches: (int, int)):
-        """Create a new query
-        
-        Arguments:
-            start_graph {GraphHandle} -- A query graph
-            end_graph {GraphHandle} -- A target graph
-            matches {(int, int)} -- a set of directed edges going from the query graph to the target graph
-        
-        Returns:
-            QueryHandle -- a new query handle
-        """
-
-        with session_scope() as session:
-            query = session.query(sqlalchemy.func.max(model.Query.query_id)).first()
-            query_id = query[0]
-            
-            if query_id is None:
-                query_id = 0
-            else:
-                query_id += 1 
-
-            new_query = model.Query(
-                query_id=query_id, start_graph_id=start_graph.graph_id, end_graph_id=end_graph.graph_id
-            )
-            session.add(new_query)
-            session.commit()
-
-            session.add_all(
-                [
-                    model.Match(
-                        start=start, 
-                        end=end, 
-                        weight=weight, 
-                        start_graph_id=start_graph.graph_id, 
-                        end_graph_id=end_graph.graph_id, 
-                        query_id=query_id
-                    )
-                    for start, end, weight in check_matches(matches)
-                ]
-            )
-            session.commit()
-        
-        return QueryHandle(query_id)
-
-    @classmethod
-    def read(cls, query_id):
-        """ create a new handle to the query with query_id `query_id`"""
-        return GraphHandle(query_id)
-
-    def delete(self):
-        """ delete this query"""
-        self.check_exists()
-        with session_scope() as session:
-            session.query(model.Match).filter(model.Match.query_id==self._query_id).delete()
-
-    def check_exists(self):
-        """ raise an exception if this query no longer exists """
-        with session_scope() as session:
-            exists = session.query(sqlalchemy.exists().where(model.Match.query_id==self._query_id)).scalar()
-        if not exists:
-            raise ValueError('cannot read query with graph id: {}'.format(self._query_id))
-
-    def __repr__(self):
-        return '<QueryHandle(query_id={})>'.format(self._query_id)
-
-    def execute(self, hopping_distance=2, max_iters=10, n=5, edges=False):
-        """Find the `n` best subgraphs in the target graph that match the query graph
-        
-        Keyword Arguments:
-            hopping_distance {int} -- Hyperparameter representing a number of hops on a graph (default: {2})
-            max_iters {int} -- maximum number of iterations before the optimiser quits (default: {10})
-            n {int} -- number of matches to return (default: {5})
-            edges {bool} -- include diagnostic information about edges (default: {False})
-        
-        Returns:
-            dict -- a query result which can be serialised as JSON
-        """
-
-        self.check_exists()
-        #TODO: support offsets
-        offsets = None
-
-        # optimisation
-        with session_scope() as session:
-            sql_query = fornax.select.join(self._query_id, h=hopping_distance, offsets=offsets)
-            records = sql_query.with_session(session).all()
-
-        inference_costs, subgraphs, iters, sz, target_edges_arr = fornax.opt.solve(
-            records, 
-            hopping_distance=hopping_distance, 
-            max_iters=max_iters
-        )
-
-        # query node meta data
-        with session_scope() as session:
-            sql_query = session.query(model.Node)
-            sql_query = sql_query.join(model.Query, model.Node.graph_id == model.Query.start_graph_id)
-            sql_query = sql_query.filter(model.Query.query_id==self._query_id)
-            query_nodes = [
-                {'id': node.node_id, 'meta':json.loads(node.meta)} 
-                if node.meta is not None
-                else {'id': node.node_id}
-                for node in sql_query.all()
-            ]
-
-        # query edge meta data
-        with session_scope() as session:
-            query_edges = None
-            if edges:
-                sql_query = session.query(model.Edge)
-                sql_query = sql_query.join(model.Query, model.Edge.graph_id == model.Query.start_graph_id)
-                sql_query = sql_query.filter(model.Query.query_id==self._query_id)
-                sql_query = sql_query.filter(model.Edge.start < model.Edge.end)
-                query_edges = [(edge.start, edge.end, 1) for edge in sql_query.all()]
-
-        # generate json for top k-matches
-        scores = []
-        for subgraph in subgraphs:
-            score = sum(inference_costs[k] for k in subgraph)
-            score += sz - len(subgraph)
-            score /= len(query_nodes)
-            scores.append(score)
-        
-        idx = sorted(enumerate(scores), key=lambda x: x[1])
-        payload = {
-            'iterations': iters, 
-            'subgraph_matches': [],
-            'query_nodes': query_nodes,
-        }
-
-        for i, score in idx[:min(n, len(idx))]:
-
-            subgraph = [
-                {
-                    #TODO: these should be represented by a list of match offsets into a seperate list of matches
-                    'query_node_offset': int(a), 
-                    'target_node_offset': int(b)
-                } 
-                for a, b in subgraphs[i]
-            ]
-            payload['subgraph_matches'].append(
-                {
-                    'subgraph_match': subgraph, 
-                    'total_score': score,
-                    'individual_scores': [
-                        float(inference_costs[match['query_node_offset'], match['target_node_offset']])
-                        for match in subgraph
-                    ]
-                } 
-            )
-
-        
-        # only include data about target nodes that appear in one of the matches
-        target_nodes = {match['target_node_offset'] for result in payload['subgraph_matches'] for match in result['subgraph_match']}
-        
-        # only include target edges that are between the target nodes above
-        between_target_nodes = lambda x: x[0] in target_nodes and x[1] in target_nodes
-        target_edges = [(int(start), int(end), int(d)) for start, end, d in target_edges_arr[['u', 'uu', 'dist_u']]]
-        target_edges = list(filter(between_target_nodes, target_edges))
-        target_nodes = list(target_nodes)
-
-        # get the mata data for target nodes
-        with session_scope() as session:
-            sql_query = session.query(model.Node.meta)
-            sql_query = sql_query.filter(model.Node.node_id.in_(target_nodes))
-            sql_query = sql_query.order_by(model.Node.node_id.desc())
-            meta_data = sql_query.all()
-        target_nodes = sorted(target_nodes, reverse=True)
-        payload['target_nodes'] = [{'id': item} for item in target_nodes]
-        for target_node, meta in zip(payload['target_nodes'], meta_data):
-            if meta[0] is not None:
-                target_node['meta'] = json.loads(meta[0])
-
-        # convert edge node ids into offsets
-        query_node_lookup = {obj['id']:i for i, obj in enumerate(list(query_nodes))}
-        target_node_lookup = {obj:i for i, obj in enumerate(list(target_nodes))}
-
-        for result in payload['subgraph_matches']:
-            result['subgraph_match'] = [
-                {
-                    'query_node_offset': query_node_lookup[item['query_node_offset']], 
-                    'target_node_offset': target_node_lookup[item['target_node_offset']]
-                } 
-                for item in result['subgraph_match']
-            ]
-
-        if edges:
-
-            payload['target_edges'] = [
-                {
-                    'start_offset': target_node_lookup[start],
-                    'end_offset': target_node_lookup[end],
-                }
-                for start, end, d in target_edges
-            ]
-
-            payload['query_edges'] = [
-                {
-                    'start_offset': query_node_lookup[start],
-                    'end_offset': query_node_lookup[end],
-                }
-                for start, end, d in query_edges
-            ]
-
-        return payload
--- test/test_api.py
+++ test/test_api.py
@@ -19,216 +19,23 @@ class TestGraph(TestCaseDB):
         self.assertRaises(ValueError, fornax.api.GraphHandle.read, 0)
 
     def test_create(self):
-        graph = fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5)))
+        graph = fornax.api.GraphHandle.create()
         self.assertEqual(graph.graph_id, 0)
 
-    def test_non_edges(self):
-        self.assertRaises(
-            ValueError,
-            fornax.api.GraphHandle.create,
-            range(5), 
-            zip(range(2), range(2))
-        )
+    def test_create_two(self):
+        _ = fornax.api.GraphHandle.create()
+        second = fornax.api.GraphHandle.create()
+        self.assertEqual(second.graph_id, 1)
 
-    def test_assert_int_edges(self):
-        self.assertRaises(
-            ValueError,
-            fornax.api.GraphHandle.create,
-            range(5), 
-            (('a', 'b'), ('c', 'd'))
-        )
-
-    def test_assert_int_node_id(self):
-        self.assertRaises(
-            ValueError,
-            fornax.api.GraphHandle.create,
-            ('a', 'b', 'c'), 
-            []
-        )
-
-    def test_count(self):
-        """ len(graph) should count the nodes in a graph """
-        N = 5
-        graph = fornax.api.GraphHandle.create(range(N), [])
-        self.assertEqual(len(graph), N)
+    def test_read(self):
+        graph = fornax.api.GraphHandle.create()
+        graph_id = graph.graph_id
+        same_graph = fornax.api.GraphHandle.read(graph_id)
+        self.assertEqual(same_graph.graph_id, graph_id)
 
     def test_delete(self):
-        graph = fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5)))
+        graph = fornax.api.GraphHandle.create()
         graph.delete()
-        self.assertEqual(len(self.session.query(fornax.model.Node).all()), 0)
-        self.assertEqual(len(self.session.query(fornax.model.Edge).all()), 0)
-
-    def test_delete_exists(self):
-            graph = fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5)))
-            graph.delete()
-            self.assertRaises(ValueError, graph.delete)
-
-    def test_increment_id(self):
-        graphs = [fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5))) for i in range(3)]
-        self.assertListEqual([g.graph_id for g in graphs], [0, 1, 2])
-
-
-class TestQuery(TestCaseDB):
-
-    @classmethod
-    def setUp(self):
-        # trick fornax into using the test database setup
-        super().setUp(self)
-        fornax.api.Session = lambda: Session(self._connection)
-
-    def test_match_start_raises(self):
-
-        graph_a = fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5)))
-        graph_b = fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5)))
-        self.assertRaises(
-            ValueError,
-            fornax.api.QueryHandle.create,
-            graph_a,
-            graph_b,
-            [('a', 1, 1), (1, 2, 1), (2, 2, 1)]
-        )   
-
-    def test_match_end_raises(self):
-
-        graph_a = fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5)))
-        graph_b = fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5)))
-        self.assertRaises(
-            ValueError,
-            fornax.api.QueryHandle.create,
-            graph_a,
-            graph_b,
-            [(1, 'a', 1), (1, 2, 1), (2, 2, 1)]
-        )
-
-    def test_match_weight_raises(self):
-
-        graph_a = fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5)))
-        graph_b = fornax.api.GraphHandle.create(range(5), zip(range(2), range(2, 5)))
-
-        self.assertRaises(
-            ValueError,
-            fornax.api.QueryHandle.create,
-            graph_a,
-            graph_b,
-            [(1, 1, 1.1), (1, 2, 1), (2, 2, 1)]
-        )
-
-        self.assertRaises(
-            ValueError,
-            fornax.api.QueryHandle.create,
-            graph_a,
-            graph_b,
-            [(1, 1, 0), (1, 2, 1), (2, 2, 1)]
-        ) 
-
-        self.assertRaises(
-            ValueError,
-            fornax.api.QueryHandle.create,
-            graph_a,
-            graph_b,
-            [(1, 1, 'b'), (1, 2, 1), (2, 2, 1)]
-        )
-
-
-class TestExecute(TestCaseDB):
-
-    @classmethod
-    def setUp(self):
-        # trick fornax into using the test database setup
-        super().setUp(self)
-        fornax.api.Session = lambda: Session(self._connection)
-
-        self.src = fornax.api.GraphHandle.create(
-            range(1, 6), 
-            [(1, 3), (1, 2), (2, 4), (4, 5)]
-        )
-
-        self.target = fornax.api.GraphHandle.create(
-            range(1, 14),
-            [
-                (1, 2), (1, 3), (1, 4),
-                (3, 7), (4, 5), (4, 6),
-                (5, 7), (6, 8), (7, 10),
-                (8, 9), (8, 12), (9, 10),
-                (10, 11), (11, 12), (11, 13),
-            ]
-        )
-
-        self.matches = [
-            (1, 1, 1), (1, 4, 1), (1, 8, 1),
-            (2, 2, 1), (2, 5, 1), (2, 9, 1),
-            (3, 3, 1), (3, 6, 1), (3, 12, 1), (3, 13, 1),
-            (4, 7, 1), (4, 10, 1),
-            (5, 11, 1)
-        ]
-
-        self.query = fornax.api.QueryHandle.create(self.src, self.target, self.matches)
-        self.payload = self.query.execute(n=2, edges=True)
-        self.results = self.payload['subgraph_matches']
-        self.subgraphs = [result['subgraph_match'] for result in self.results]
-
-    def test_lim(self):
-        self.assertEqual(len(self.subgraphs), 2)
-
-    def test_scores(self):
-            self.assertListEqual(
-                [result['total_score'] for result in self.results],
-                [0 for _ in self.results]
-            )
-
-    def test_subgraph_matches(self):
-        self.assertIn(
-            [
-                {'query_node_offset': 0, 'target_node_offset': 4}, 
-                {'query_node_offset': 1, 'target_node_offset': 3}, 
-                {'query_node_offset': 2, 'target_node_offset': 5}, 
-                {'query_node_offset': 3, 'target_node_offset': 2}, 
-                {'query_node_offset': 4, 'target_node_offset': 1}
-            ],
-            self.subgraphs
-        )
-        self.assertIn(
-            [
-                {'query_node_offset': 0, 'target_node_offset': 4}, 
-                {'query_node_offset': 1, 'target_node_offset': 3}, 
-                {'query_node_offset': 2, 'target_node_offset': 0}, 
-                {'query_node_offset': 3, 'target_node_offset': 2}, 
-                {'query_node_offset': 4, 'target_node_offset': 1}
-            ], 
-            self.subgraphs
-        )
-
-    def test_query_nodes(self):
-        self.assertListEqual(
-            self.payload['query_nodes'], 
-            [{'id': 1}, {'id': 2}, {'id': 3}, {'id': 4}, {'id': 5}]
-        )
-    
-    def test_query_edges(self):
-        self.assertListEqual(
-            self.payload['query_edges'], 
-            [
-                {'end_offset': 1, 'start_offset': 0},
-                {'end_offset': 2, 'start_offset': 0},
-                {'end_offset': 3, 'start_offset': 1},
-                {'end_offset': 4, 'start_offset': 3}
-            ]
-        )
-    
-    def test_target_nodes(self):
-        self.assertListEqual(
-            self.payload['target_nodes'],
-            [{'id': 12}, {'id': 11}, {'id': 10}, {'id': 9}, {'id': 8}, {'id': 6}]
-        )
-
-    def test_target_edges(self):
-        self.assertListEqual(
-            self.payload['target_edges'],
-            [
-                {'end_offset': 4, 'start_offset': 5}, 
-                {'end_offset': 3, 'start_offset': 4}, 
-                {'end_offset': 0, 'start_offset': 4}, 
-                {'end_offset': 2, 'start_offset': 3}, 
-                {'end_offset': 1, 'start_offset': 2}
-            ]
-        )
+        self.assertRaises(ValueError, fornax.api.GraphHandle.read, 0)
+        
+        
