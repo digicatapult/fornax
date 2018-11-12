14f0258f226d06a9dd1b8fd3e59802968fe05e66
Dan-Staff <daniel.r.staff@gmail.com>
2018-11-12 15:56:31 +0000

    graph handles are namespaced directly under fornax

--- test/test_api.py
+++ test/test_api.py
@@ -16,38 +16,38 @@ class TestGraph(TestCaseDB):
 
     def test_init_raises(self):
         """ raise an ValueError if a hadle to a graph is constructed that does not exist """
-        self.assertRaises(ValueError, fornax.api.GraphHandle, 0)
-        self.assertRaises(ValueError, fornax.api.GraphHandle.read, 0)
+        self.assertRaises(ValueError, fornax.GraphHandle, 0)
+        self.assertRaises(ValueError, fornax.GraphHandle.read, 0)
 
     def test_create(self):
-        graph = fornax.api.GraphHandle.create()
+        graph = fornax.GraphHandle.create()
         self.assertEqual(graph.graph_id, 0)
 
     def test_create_two(self):
         _ = fornax.api.GraphHandle.create()
-        second = fornax.api.GraphHandle.create()
+        second = fornax.GraphHandle.create()
         self.assertEqual(second.graph_id, 1)
 
     def test_read(self):
         graph = fornax.api.GraphHandle.create()
         graph_id = graph.graph_id
-        same_graph = fornax.api.GraphHandle.read(graph_id)
+        same_graph = fornax.GraphHandle.read(graph_id)
         self.assertEqual(same_graph.graph_id, graph_id)
 
     def test_delete(self):
-        graph = fornax.api.GraphHandle.create()
+        graph = fornax.GraphHandle.create()
         graph.delete()
         self.assertRaises(ValueError, fornax.api.GraphHandle.read, 0)
 
     def test_add_nodes(self):
-        graph = fornax.api.GraphHandle.create()
+        graph = fornax.GraphHandle.create()
         names = ['adam', 'ben', 'chris']
         graph.add_nodes(name=names)
         nodes = self.session.query(fornax.model.Node).filter(fornax.model.Node.graph_id==0).all()
         self.assertListEqual(names, [json.loads(node.meta)['name'] for node in nodes])
 
     def test_add_nodes_more_meta(self):
-        graph = fornax.api.GraphHandle.create()
+        graph = fornax.GraphHandle.create()
         names = ['adam', 'ben', 'chris']
         ages = [9, 10 ,11]
         graph.add_nodes(name=names, age=ages)
@@ -56,12 +56,12 @@ class TestGraph(TestCaseDB):
         self.assertListEqual(ages, [json.loads(node.meta)['age'] for node in nodes])
         
     def test_missing_attribute(self):
-        graph = fornax.api.GraphHandle.create()
+        graph = fornax.GraphHandle.create()
         names = ['adam', 'ben', 'chris']
         ages = [9, 10]
         self.assertRaises(TypeError, graph.add_nodes, name=names, age=ages)
 
     def test_assign_id(self):
-        graph = fornax.api.GraphHandle.create()
+        graph = fornax.GraphHandle.create()
         ids = range(3)
         self.assertRaises(ValueError, graph.add_nodes, id=ids)
