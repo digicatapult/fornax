05dcfb534e6939ab1eead30beef3e53fb14e9ed0
Dan-Staff <daniel.r.staff@gmail.com>
2018-11-19 11:54:25 +0000

    PEP8 formatting setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open('README.md') as fp:
     long_description = fp.read()
 
 setup(
-    name = 'fornax',
-    version = fornax.__version__,
-    license = 'Apache License 2.0',
-    author = 'Daniel Staff',
-    description = 'Approximate fuzzy subgraph matching in polynomial time',
-    install_requires = [
+    name='fornax',
+    version=fornax.__version__,
+    license='Apache License 2.0',
+    author='Daniel Staff',
+    description='Approximate fuzzy subgraph matching in polynomial time',
+    install_requires=[
         'psycopg2>=2.7.4',
         'SQLAlchemy>=1.2.8',
         'numpy>-1.14.5'
     ],
-    author_email = 'daniel.staff@digicatapult.org.uk',
-    long_description = long_description,
-    packages = find_packages(),
-    classifiers = [
+    author_email='daniel.staff@digicatapult.org.uk',
+    long_description=long_description,
+    packages=find_packages(),
+    classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
