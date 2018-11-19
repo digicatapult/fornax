54461d7304cd25394d7d34183d3355620758006f
Dan-Staff <daniel.r.staff@gmail.com>
2018-11-19 11:53:16 +0000

    PEP8 linting in model.py

--- fornax/model.py
+++ fornax/model.py
@@ -21,9 +21,12 @@ class Query(Base):
     )
 
     query_id = Column(Integer, primary_key=True)
-    start_graph_id = Column(Integer, ForeignKey("graph.graph_id"), nullable=False, index=True)
-    end_graph_id = Column(Integer, ForeignKey("graph.graph_id"), nullable=False, index=True)
-    Index('query_idx', 'query_id', 'start_graph_id', 'end_graph_id', unique=True)
+    start_graph_id = Column(
+        Integer, ForeignKey("graph.graph_id"), nullable=False, index=True)
+    end_graph_id = Column(
+        Integer, ForeignKey("graph.graph_id"), nullable=False, index=True)
+    Index(
+        'query_idx', 'query_id', 'start_graph_id', 'end_graph_id', unique=True)
 
 
 class Match(Base):
@@ -31,12 +34,17 @@ class Match(Base):
 
     __tablename__ = 'match'
     __table_args__ = (
-        PrimaryKeyConstraint('query_id', 'start_graph_id', 'end_graph_id', 'start', 'end'),
-        ForeignKeyConstraint(['start_graph_id', 'start'], ['node.graph_id', 'node.node_id'], name="fk_match_start"),
-        ForeignKeyConstraint(['end_graph_id', 'end'], ['node.graph_id', 'node.node_id'], name="fk_match_end"),
+        PrimaryKeyConstraint(
+            'query_id', 'start_graph_id', 'end_graph_id', 'start', 'end'),
         ForeignKeyConstraint(
-            ['query_id', 'start_graph_id', 'end_graph_id'], 
-            ['query.query_id', 'query.start_graph_id', 'query.end_graph_id'], 
+            ['start_graph_id', 'start'], ['node.graph_id', 'node.node_id'],
+            name="fk_match_start"),
+        ForeignKeyConstraint(
+            ['end_graph_id', 'end'], ['node.graph_id', 'node.node_id'],
+            name="fk_match_end"),
+        ForeignKeyConstraint(
+            ['query_id', 'start_graph_id', 'end_graph_id'],
+            ['query.query_id', 'query.start_graph_id', 'query.end_graph_id'],
             name="fk_query"
         )
     )
@@ -48,27 +56,32 @@ class Match(Base):
     query_id = Column(Integer)
     meta = Column(String, nullable=True)
 
-    weight = Column(Float, 
+    weight = Column(
+        Float,
         CheckConstraint("weight>0", name="min_check"),
         CheckConstraint("weight<=1", name="max_check"),
         nullable=False
     )
-    
+
     start_node = relationship(
-        'Node', 
-        primaryjoin="and_(Match.start == Node.node_id, Match.start_graph_id == Node.node_id)", 
+        'Node',
+        primaryjoin="and_(Match.start == Node.node_id, \
+        Match.start_graph_id == Node.node_id)",
         backref="start_matches"
     )
-    
+
     end_node = relationship(
         'Node',
-        primaryjoin="and_(Match.end == Node.node_id, Match.end_graph_id == Node.graph_id)",
+        primaryjoin="and_(Match.end == Node.node_id, \
+        Match.end_graph_id == Node.graph_id)",
         backref="end_matches"
     )
 
     def __repr__(self):
-        return "<Match(start={}, end={}, weight={}, start_graph_id={}, end_graph_id={}, query={})>".format(
-            self.start, self.end, self.weight, self.start_graph_id, self.end_graph_id, self.query_id
+        return "<Match(start={}, end={}, weight={}, start_graph_id={}, \
+        end_graph_id={}, query={})>".format(
+            self.start, self.end, self.weight, self.start_graph_id,
+            self.end_graph_id, self.query_id
         )
 
 
@@ -79,15 +92,20 @@ class Node(Base):
     __table_args__ = (
         PrimaryKeyConstraint('graph_id', 'node_id'),
     )
-    node_id = Column(Integer, CheckConstraint("node_id>=0", name="q_min_id_check"))
+    node_id = Column(
+        Integer,
+        CheckConstraint("node_id>=0", name="q_min_id_check")
+    )
     graph_id = Column(Integer, ForeignKey("graph.graph_id"))
     meta = Column(String, nullable=True)
-    
+
     def neighbours(self):
         return [x.end_node for x in self.start_edges]
 
     def __repr__(self):
-        return "<Node(node_id={}, graph_id={})>".format(self.node_id, self.graph_id)
+        return "<Node(node_id={}, graph_id={})>".format(
+            self.node_id, self.graph_id
+        )
 
 
 class Edge(Base):
@@ -96,8 +114,14 @@ class Edge(Base):
     __tablename__ = 'edge'
     __table_args__ = (
         PrimaryKeyConstraint('graph_id', 'start', 'end'),
-        ForeignKeyConstraint(['graph_id', 'start'], ['node.graph_id', 'node.node_id']),
-        ForeignKeyConstraint(['graph_id', 'end'], ['node.graph_id', 'node.node_id'])
+        ForeignKeyConstraint(
+            ['graph_id', 'start'],
+            ['node.graph_id', 'node.node_id']
+        ),
+        ForeignKeyConstraint(
+            ['graph_id', 'end'],
+            ['node.graph_id', 'node.node_id']
+        )
     )
 
     start = Column(Integer)
@@ -106,16 +130,19 @@ class Edge(Base):
     meta = Column(String, nullable=True)
 
     start_node = relationship(
-        'Node', 
-        primaryjoin='and_(Node.node_id == Edge.start, Node.graph_id == Edge.graph_id)', 
+        'Node',
+        primaryjoin='and_(Node.node_id == Edge.start, \
+        Node.graph_id == Edge.graph_id)',
         backref='start_edges'
     )
 
     end_node = relationship(
-        'Node', 
-        primaryjoin='and_(Node.node_id == Edge.end, Node.graph_id == Edge.graph_id)',
+        'Node',
+        primaryjoin='and_(Node.node_id == Edge.end, \
+        Node.graph_id == Edge.graph_id)',
         backref='end_edges'
     )
-    
+
     def __repr__(self):
-        return "<Edge(start={}, end={}, graph_id={})>".format(self.start, self.end, self.graph_id)
+        return "<Edge(start={}, end={}, graph_id={})>".format(
+            self.start, self.end, self.graph_id)
