bf5321f345ab5436e00c9143cb3ca74e5d102adb
Dan-Staff <daniel.r.staff@gmail.com>
2019-01-04 16:25:07 +0000

    run_test should have !0  exit code if tests fail

--- run_test.py
+++ run_test.py
@@ -2,8 +2,10 @@
 import xmlrunner
 import unittest
 import os
+import sys
 
 if __name__ == '__main__':
 
     tests = unittest.TestLoader().discover('test')
-    xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS','test-reports')).run(tests)
+    results = xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS', 'test-reports')).run(tests)
+    sys.exit(not results.wasSuccessful())
